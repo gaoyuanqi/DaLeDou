501d7d21abfcc9143897298495a747fb7f2810a2
‘雨园’ <2278612931@qq.com>
2022-10-27 14:35:46 +0800

    add mygang.py

--- daledou/daledouone.py
+++ daledou/daledouone.py
@@ -600,77 +600,8 @@ class DaLeDouOne(DaLeDou):
 
     @deco
     def 我的帮派(self):
-        self.msg += DaLeDou.conversion('我的帮派')
-
-        # 帮派任务
-        DaLeDouOne.get('cmd=factiontask&sub=1')
-        faction_missions = html
-
-        missions = {
-            '帮战冠军': 'cmd=facwar&sub=4',
-            '查看帮战': 'cmd=facwar&sub=4',
-            '查看帮贡': 'cmd=factionhr&subtype=14',
-            '查看祭坛': 'cmd=altar',
-            '查看踢馆': 'cmd=facchallenge&subtype=0',
-            '查看要闻': 'cmd=factionop&subtype=8&pageno=1&type=2',
-            '加速贡献': 'cmd=use&id=3038&store_type=1&page=1',
-            '粮草掠夺': 'cmd=forage_war',
-        }
-        for name, url in missions.items():
-            if name in faction_missions:
-                DaLeDouOne.get(url)
-
-        if '帮派修炼' in faction_missions:
-            # 2727  紫霞秘籍
-            # 2758  逍遥心法
-            # 2505  铁砂掌
-            # 2536  真元护体
-            # 2437  九阳神功
-            # 2442  金钟罩
-            # 2377  北冥神功
-            # 2399  乾坤大挪移
-            # 2429  养生之道
-            n = 0
-            for id in [2727, 2758, 2505, 2536, 2437, 2442, 2377, 2399, 2429]:
-                for _ in range(4):
-                    DaLeDouOne.get(
-                        f'cmd=factiontrain&type=2&id={id}&num=1&i_p_w=num%7C')
-                    if '你需要提升帮派等级来让你进行下一步的修炼' in html:
-                        if id == 2429:
-                            self.msg += ['所有武功秘籍已满级']
-                            return
-                        break
-                    elif '技能经验增加' in html:
-                        # 技能经验增加20！
-                        n += 1
-                        self.msg += DaLeDou.findall(
-                            r'规则说明</a><br />(.*?)<br />')
-                    else:
-                        break
-                if n == 4:
-                    break
-
-        if self.week == '0':
-            for sub in [4, 9, 6]:
-                # 领取奖励 》报名帮派战争 》激活祝福
-                DaLeDouOne.get(f'cmd=facwar&sub={sub}')
-                self.msg += DaLeDou.findall(r'</p>(.*?)<br /><a.*?查看上届')
-
-        for _ in range(5):
-            # 供奉守护神
-            DaLeDouOne.get('cmd=viewolbation')
-            text_list = DaLeDou.findall(r'数量：.*?id=(\d+).*?供奉</a>')
-            # 供奉
-            DaLeDouOne.get(f'cmd=oblation&id={text_list[0]}&page=1')
-            self.msg += DaLeDou.findall(r'【供奉守护神】<br />(.*?)<br />')
-
-        # 帮派任务
-        DaLeDouOne.get('cmd=factiontask&sub=1')
-        text_list = DaLeDou.findall(r'id=(\d+)">领取奖励</a>')
-        for id in text_list:
-            # 领取奖励
-            DaLeDouOne.get(f'cmd=factiontask&sub=3&id={id}')
-        self.msg += DaLeDou.findall_tuple(r'id=\d+">(.*?)</a>(.*?)<br />')
+        from daledou.mygang import MyGang
+        self.msg += MyGang().main()
 
     @deco
     def 帮派祭坛(self):
---
+++ daledou/mygang.py
@@ -0,0 +1,102 @@
+'''
+我的帮派
+'''
+from daledou.daledou import DaLeDou
+
+
+class MyGang(DaLeDou):
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def get(params: str):
+        global html
+        html = DaLeDou.get(params)
+
+    def 供奉(self):
+        '''
+        【背包】部分靠前物品会被供奉
+        '''
+        for _ in range(5):
+            # 供奉守护神
+            MyGang.get('cmd=viewolbation')
+            text_list = DaLeDou.findall(r'数量：.*?id=(\d+).*?供奉</a>')
+            # 供奉
+            MyGang.get(f'cmd=oblation&id={text_list[0]}&page=1')
+            self.msg += DaLeDou.findall(r'【供奉守护神】<br />(.*?)<br />')
+
+    def 帮战(self):
+        '''
+        周日 领取奖励 》报名帮派战争 》激活祝福
+        '''
+        if self.week == '0':
+            for sub in [4, 9, 6]:
+                MyGang.get(f'cmd=facwar&sub={sub}')
+                self.msg += DaLeDou.findall(r'</p>(.*?)<br /><a.*?查看上届')
+
+    def 帮派任务(self):
+        '''
+        2727  紫霞秘籍
+        2758  逍遥心法
+        2505  铁砂掌
+        2536  真元护体
+        2437  九阳神功
+        2442  金钟罩
+        2377  北冥神功
+        2399  乾坤大挪移
+        2429  养生之道
+        '''
+        # 帮派任务
+        MyGang.get('cmd=factiontask&sub=1')
+        faction_missions = html
+
+        missions = {
+            '帮战冠军': 'cmd=facwar&sub=4',
+            '查看帮战': 'cmd=facwar&sub=4',
+            '查看帮贡': 'cmd=factionhr&subtype=14',
+            '查看祭坛': 'cmd=altar',
+            '查看踢馆': 'cmd=facchallenge&subtype=0',
+            '查看要闻': 'cmd=factionop&subtype=8&pageno=1&type=2',
+            '加速贡献': 'cmd=use&id=3038&store_type=1&page=1',
+            '粮草掠夺': 'cmd=forage_war',
+        }
+        for name, url in missions.items():
+            if name in faction_missions:
+                MyGang.get(url)
+
+        if '帮派修炼' in faction_missions:
+            n = 0
+            for id in [2727, 2758, 2505, 2536, 2437, 2442, 2377, 2399, 2429]:
+                for _ in range(4):
+                    MyGang.get(
+                        f'cmd=factiontrain&type=2&id={id}&num=1&i_p_w=num%7C')
+                    if '你需要提升帮派等级来让你进行下一步的修炼' in html:
+                        if id == 2429:
+                            self.msg += ['所有武功秘籍已满级']
+                        break
+                    elif '技能经验增加' in html:
+                        # 技能经验增加20！
+                        n += 1
+                        self.msg += DaLeDou.findall(
+                            r'规则说明</a><br />(.*?)<br />')
+                if n == 4:
+                    break
+
+        # 帮派任务
+        MyGang.get('cmd=factiontask&sub=1')
+        text_list = DaLeDou.findall(r'id=(\d+)">领取奖励</a>')
+        for id in text_list:
+            # 领取奖励
+            MyGang.get(f'cmd=factiontask&sub=3&id={id}')
+        self.msg += DaLeDou.findall_tuple(r'id=\d+">(.*?)</a>(.*?)<br />')
+
+    def main(self) -> list[str]:
+        self.msg += DaLeDou.conversion('我的帮派')
+
+        self.供奉()
+        self.帮战()
+        self.帮派任务()
+
+        # [2:] 表示切掉多余的 ['【开始时间】', '2022-10-22 21:26:34']
+        return self.msg[2:]
