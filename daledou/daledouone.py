c60fc6920715ff4acadcc1b3225bdd6e75bfcc3d
‘雨园’ <2278612931@qq.com>
2022-10-27 14:07:14 +0800

    新增 menpai.py

--- daledou/daledouone.py
+++ daledou/daledouone.py
@@ -361,74 +361,8 @@ class DaLeDouOne(DaLeDou):
 
     @deco
     def 门派(self):
-        self.msg += DaLeDou.conversion('门派')
-
-        # 万年寺: 点燃 》点燃
-        for op in ['fumigatefreeincense', 'fumigatepaidincense']:
-            DaLeDouOne.get(f'cmd=sect&op={op}')
-            self.msg += DaLeDou.findall(r'修行。<br />(.*?)<br />')
-
-        # 八叶堂:进入木桩训练 》进入同门切磋
-        for op in ['trainingwithnpc', 'trainingwithmember']:
-            DaLeDouOne.get(f'cmd=sect&op={op}')
-            self.msg += DaLeDou.findall(r'【八叶堂】<br />(.*?)<br />')
-
-        # 五花堂
-        DaLeDouOne.get('cmd=sect_task')
-        wuhuatang = html
-
-        missions = {
-            '进入华藏寺看一看': 'cmd=sect_art',
-            '进入伏虎寺看一看': 'cmd=sect_trump',
-            '进入金顶看一看': 'cmd=sect&op=showcouncil',
-            '进入八叶堂看一看': 'cmd=sect&op=showtraining',
-            '进入万年寺看一看': 'cmd=sect&op=showfumigate'
-        }
-        for name, url in missions.items():
-            if name in wuhuatang:
-                DaLeDouOne.get(url)
-
-        if '进行一次武艺切磋' in wuhuatang:
-            for rank in [1, 2, 3]:
-                # 掌门 首座 堂主
-                DaLeDouOne.get(
-                    f'cmd=sect&op=trainingwithcouncil&rank={rank}&pos=1')
-
-        if '查看一名' in wuhuatang:
-            # 查看一名同门成员的资料 or 查看一名其他门派成员的资料
-            for page in [2, 3]:
-                # 好友第2、3页
-                DaLeDouOne.get(f'cmd=friendlist&page={page}')
-                uin_list = DaLeDou.findall(r'\d+：.*?B_UID=(\d+).*?级')
-                for uin in uin_list:
-                    # 查看好友
-                    DaLeDouOne.get(f'cmd=totalinfo&B_UID={uin}')
-
-        if '进行一次心法修炼' in wuhuatang:
-            '''
-            少林心法      峨眉心法    华山心法      丐帮心法    武当心法      明教心法
-            101 法华经    104 斩情决  107 御剑术   110 醉拳    113 太极内力  116 圣火功
-            102 金刚经    105 护心决  108 龟息术   111 烟雨行  114 绕指柔剑  117 五行阵
-            103 达摩心经  106 观音咒  109 养心术   112 笑尘诀  115 金丹秘诀  118 日月凌天
-            '''
-            for id in range(101, 119):
-                DaLeDouOne.get(f'cmd=sect_art&subtype=2&art_id={id}&times=1')
-                if '修炼成功' in html:
-                    self.msg += DaLeDou.findall(r'【心法修炼】<br />(.*?)<br />')
-                    break
-                elif '修炼失败' in html:
-                    if '你的门派贡献不足无法修炼' in html:
-                        break
-                    elif ('你的心法已达顶级无需修炼' in html) and (id == 118):
-                        self.msg += ['所有心法都已经顶级']
-
-        # 五花堂
-        DaLeDouOne.get('cmd=sect_task')
-        text_list = DaLeDou.findall(r'task_id=(\d+)">完成')
-        for id in text_list:
-            # 完成
-            DaLeDouOne.get(f'cmd=sect_task&subtype=2&task_id={id}')
-            self.msg += DaLeDou.findall(r'【五花堂】<br />(.*?)<br /><br />')
+        from daledou.menpai import MenPai
+        self.msg += MenPai().main()
 
     @deco
     def 门派邀请赛(self):
@@ -767,15 +701,6 @@ class DaLeDouOne(DaLeDou):
             elif '领取奖励' in html:
                 # 领取奖励
                 DaLeDouOne.get('cmd=altar&op=drawreward')
-
-    @deco
-    def 大侠回归三重好礼(self):
-        self.msg += DaLeDou.conversion('大侠回归三重好礼')
-        DaLeDouOne.get('cmd=newAct&subtype=173&op=1')
-        text_list = DaLeDou.findall(r'天.*?taskid=(\d+)">领取')
-        for id in text_list:
-            # 领取
-            DaLeDouOne.get(f'cmd=newAct&subtype=173&op=2&taskid={id}')
 
     @deco
     def 飞升大作战(self):
---
+++ daledou/menpai.py
@@ -0,0 +1,96 @@
+'''
+门派
+'''
+from daledou.daledou import DaLeDou
+
+
+class MenPai(DaLeDou):
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def get(params: str):
+        global html
+        html = DaLeDou.get(params)
+
+    def 万年寺(self):
+        # 点燃 》点燃
+        for op in ['fumigatefreeincense', 'fumigatepaidincense']:
+            MenPai.get(f'cmd=sect&op={op}')
+            self.msg += DaLeDou.findall(r'修行。<br />(.*?)<br />')
+
+    def 八叶堂(self):
+        # 进入木桩训练 》进入同门切磋
+        for op in ['trainingwithnpc', 'trainingwithmember']:
+            MenPai.get(f'cmd=sect&op={op}')
+            self.msg += DaLeDou.findall(r'【八叶堂】<br />(.*?)<br />')
+
+    def 五花堂(self):
+
+        # 五花堂
+        MenPai.et('cmd=sect_task')
+        wuhuatang = html
+
+        missions = {
+            '进入华藏寺看一看': 'cmd=sect_art',
+            '进入伏虎寺看一看': 'cmd=sect_trump',
+            '进入金顶看一看': 'cmd=sect&op=showcouncil',
+            '进入八叶堂看一看': 'cmd=sect&op=showtraining',
+            '进入万年寺看一看': 'cmd=sect&op=showfumigate'
+        }
+        for name, url in missions.items():
+            if name in wuhuatang:
+                MenPai.et(url)
+
+        if '进行一次武艺切磋' in wuhuatang:
+            for rank in [1, 2, 3]:
+                # 掌门 首座 堂主
+                MenPai.et(
+                    f'cmd=sect&op=trainingwithcouncil&rank={rank}&pos=1')
+
+        if '查看一名' in wuhuatang:
+            # 查看一名同门成员的资料 or 查看一名其他门派成员的资料
+            for page in [2, 3]:
+                # 好友第2、3页
+                MenPai.et(f'cmd=friendlist&page={page}')
+                uin_list = DaLeDou.findall(r'\d+：.*?B_UID=(\d+).*?级')
+                for uin in uin_list:
+                    # 查看好友
+                    MenPai.et(f'cmd=totalinfo&B_UID={uin}')
+
+        if '进行一次心法修炼' in wuhuatang:
+            '''
+            少林心法      峨眉心法    华山心法      丐帮心法    武当心法      明教心法
+            101 法华经    104 斩情决  107 御剑术   110 醉拳    113 太极内力  116 圣火功
+            102 金刚经    105 护心决  108 龟息术   111 烟雨行  114 绕指柔剑  117 五行阵
+            103 达摩心经  106 观音咒  109 养心术   112 笑尘诀  115 金丹秘诀  118 日月凌天
+            '''
+            for id in range(101, 119):
+                MenPai.et(f'cmd=sect_art&subtype=2&art_id={id}&times=1')
+                if '修炼成功' in html:
+                    self.msg += DaLeDou.findall(r'【心法修炼】<br />(.*?)<br />')
+                    break
+                elif '修炼失败' in html:
+                    if '你的门派贡献不足无法修炼' in html:
+                        break
+                    elif ('你的心法已达顶级无需修炼' in html) and (id == 118):
+                        self.msg += ['所有心法都已经顶级']
+
+        # 五花堂
+        MenPai.et('cmd=sect_task')
+        text_list = DaLeDou.findall(r'task_id=(\d+)">完成')
+        for id in text_list:
+            # 完成
+            MenPai.et(f'cmd=sect_task&subtype=2&task_id={id}')
+            self.msg += DaLeDou.findall(r'【五花堂】<br />(.*?)<br /><br />')
+
+    def main(self) -> list[str]:
+        self.msg += DaLeDou.conversion('门派')
+
+        self.万年寺()
+        self.八叶堂()
+        self.五花堂()
+
+        # [2:] 表示切掉多余的 ['【开始时间】', '2022-10-22 21:26:34']
+        return self.msg[2:]
