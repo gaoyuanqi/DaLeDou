e1808812b73676d1925da306d91651311a92edd4
雨园 <22786129312qq.com>
2022-11-07 13:37:02 +0800

    优化【每日奖励】通知

--- missions/daledou/meirijiangli.py
+++ missions/daledou/meirijiangli.py
@@ -16,14 +16,13 @@ class MeiRi(DaLeDou):
         html = DaLeDou.get(params)
 
     def 每日奖励(self):
-        self.msg += DaLeDou.conversion('每日奖励')
         for key in ['login', 'meridian', 'daren', 'wuzitianshu']:
             # 每日奖励
             MeiRi.get(f'cmd=dailygift&op=draw&key={key}')
             self.msg += DaLeDou.findall(r'【每日奖励】<br />(.*?)<br />')
 
     def main(self) -> list[str]:
-        self.msg += DaLeDou.conversion('深渊之潮')
+        self.msg += DaLeDou.conversion('每日奖励')
 
         self.每日奖励()
 
