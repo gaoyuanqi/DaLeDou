112add602a4b6ed46323dec1338a8ee11db43513
雨园 <22786129312qq.com>
2022-11-25 21:30:01 +0800

    新增【梦想之旅】领取超级礼包

--- missions/daledou/mengxiangzhilv.py
+++ missions/daledou/mengxiangzhilv.py
@@ -67,9 +67,11 @@ class MengXiang(DaLeDou):
         if self.week == '4':
             # 梦想之旅
             MengXiang.get('cmd=dreamtrip')
-            if text_list := DaLeDou.findall(r'sub=4&amp;bmapid=(\d+)'):
-                # 礼包
-                MengXiang.get(f'cmd=dreamtrip&sub=4&bmapid={text_list[0]}')
+            text_list = DaLeDou.findall(r'sub=4&amp;bmapid=(\d+)')
+            for id in text_list:
+                # 礼包     1 or 2 or 3 or 4
+                # 超级礼包 0
+                MengXiang.get(f'cmd=dreamtrip&sub=4&bmapid={id}')
                 self.msg += DaLeDou.findall(r'规则</a><br />(.*?)<br />')
 
     def main(self) -> list[str]:
