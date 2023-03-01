9a558ae071096821c3f257ab60d97af368881395
gaoyuanqi <2278612931@qq.com>
2023-03-01 13:21:19 +0800

    【兵法】执行时间调整

--- bingfa.py
+++ bingfa.py
@@ -31,13 +31,29 @@ class BingFa(DaLeDou):
         BingFa.get('cmd=brofight&subtype=13&op=draw')
         self.msg += DaLeDou.findall(r'领奖</a><br />(.*?)<br /><br />')
 
+    def 领斗币(self):
+        '''
+        领取斗币
+        '''
+        for t in range(1, 6):
+            BingFa.get(f'cmd=brofight&subtype=10&type={t}')
+            champion_uin: list = DaLeDou.findall(
+                r'50000&nbsp;&nbsp;(\d+).*?champion_uin=(\d+)')
+            for number, uin in champion_uin:
+                if number == '0':
+                    continue
+                BingFa.get(
+                    f'cmd=brofight&subtype=10&op=draw&champion_uin={uin}&type={t}')
+                self.msg += DaLeDou.findall(r'排行</a><br />(.*?)<br />')
+                return
+
     def run(self) -> list:
         self.msg += DaLeDou.conversion('兵法')
 
-        self.领斗币()
         if self.week == '4':
             self.助威()
         if self.week == '6':
             self.领奖()
+            self.领斗币()
 
         return self.msg
