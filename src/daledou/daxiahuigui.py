fd5cfb32ee40a9b371f3543a7883378ca8921a2e
gaoyuanqi <2278612931@qq.com>
2023-06-22 14:06:57 +0800

    V2.8.5 修复【大侠回归三重好礼】`TypeError` 错误

--- src/daledou/daxiahuigui.py
+++ src/daledou/daxiahuigui.py
@@ -15,13 +15,12 @@ class DaXia(DaLeDou):
     def run(self) -> list:
         # 大侠回归三重好礼
         DaXia.get('cmd=newAct&subtype=173&op=1')
-        two_tuple = DaLeDou.findall(r'subtype=(\d+).*?taskid=(\d+)')
-        if not two_tuple:
+        if data := DaLeDou.findall(r'subtype=(\d+).*?taskid=(\d+)'):
+            for s, t in data:
+                # 领取
+                DaXia.get(f'cmd=newAct&subtype={s}&op=2&taskid={t}')
+                self.msg.append(DaLeDou.search(r'】<br /><br />(.*?)<br />'))
+        else:
             self.msg.append('没有可领取的奖励')
-            return
-        for s, t in two_tuple:
-            # 领取
-            DaXia.get(f'cmd=newAct&subtype={s}&op=2&taskid={t}')
-            self.msg.append(DaLeDou.search(r'】<br /><br />(.*?)<br />'))
 
         return self.msg
